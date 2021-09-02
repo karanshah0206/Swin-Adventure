2439e527875bb495d729bee4eccfd9ca8bfaa38f
Karan Shah <karanshah0206@outlook.com>
2021-09-02 10:25:19 +0530

    Added Functionality To Inventory Class

--- Swin-Adventure/Inventory.cs
+++ Swin-Adventure/Inventory.cs
@@ -10,20 +10,43 @@ namespace Swin_Adventure
         { }
 
         public bool HasItem(string id)
-        { return false; }
+        {
+            foreach (Item item in _items)
+                if (item.AreYou(id))
+                    return true;
+            return false;
+        }
 
         public void Put(Item itm)
-        { }
+        { _items.Add(itm); }
 
         public Item Take(string id)
-        { return _items[0]; }
+        {
+            foreach (Item item in _items)
+                if (item.AreYou(id))
+                {
+                    _items.Remove(item);
+                    return item;
+                }
+            return null;
+        }
 
         public Item Fetch(string id)
-        { return _items[0]; }
+        {
+            foreach (Item item in _items)
+                if (item.AreYou(id))
+                    return item;
+            return null;
+        }
 
         public string ItemList
         {
-            get { return ""; }
+            get {
+                string msg = "";
+                foreach (Item item in _items)
+                    msg += "\t" + item.ShortDescription + "\n";
+                return msg;
+            }
         }
     }
 }
