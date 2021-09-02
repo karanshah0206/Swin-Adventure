f4e6660f61cb09670b5122270c672dfb0a15596e
Karan Shah <karanshah0206@outlook.com>
2021-09-02 09:40:55 +0530

    Game Object Framework Added

---
+++ Swin-Adventure/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Swin_Adventure
+{
+    abstract class Game
+    {
+        private string _name, _description;
+
+        public Game(string[] ids, string name, string desc)
+        {
+            _name = name;
+            _description = desc;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string ShortDescription
+        {
+            get { return ""; }
+        }
+
+        virtual public string FullDescription
+        {
+            get { return ""; }
+        }
+    }
+}
