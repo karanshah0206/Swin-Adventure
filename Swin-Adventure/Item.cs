7889f74e244c871cb8c4ea658821f7d3a3e4fee9
Karan Shah <karanshah0206@outlook.com>
2021-09-02 09:52:32 +0530

    Added Item Class To Store Each Item As An Object

---
+++ Swin-Adventure/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swin_Adventure
+{
+    class Item : Game
+    {
+        public Item(string[] idents, string name, string desc) : base(idents, name, desc)
+        { }
+    }
+}
