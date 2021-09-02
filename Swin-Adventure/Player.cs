94e29e7bfc233e2cdf6c3e51dde2830eba0b1817
Karan Shah <karanshah0206@outlook.com>
2021-09-02 10:54:51 +0530

    Framework for Player Class Created

---
+++ Swin-Adventure/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swin_Adventure
+{
+    class Player : Game
+    {
+        private Inventory _inventory = new Inventory();
+
+        public Player(string name, string desc) : base(new string[] { "me", "inventory" }, name, desc)
+        { }
+
+        public Game Locate(string id)
+        { return null; }
+
+        public override string FullDescription
+        {
+            get { return ""; }
+        }
+
+        public Inventory Inventory
+        {
+            get { return _inventory; }
+        }
+    }
+}
