b770b18c7b62ab7fdeff424dc9ff319243635a72
Karan Shah <karanshah0206@outlook.com>
2021-09-02 10:05:11 +0530

    Framework for Inventory Class Added

---
+++ Swin-Adventure/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Swin_Adventure
+{
+    class Inventory
+    {
+        private List<Item> _items = new List<Item>();
+
+        public Inventory()
+        { }
+
+        public bool HasItem(string id)
+        { return false; }
+
+        public void Put(Item itm)
+        { }
+
+        public Item Take(string id)
+        { return _items[0]; }
+
+        public Item Fetch(string id)
+        { return _items[0]; }
+
+        public string ItemList
+        {
+            get { return ""; }
+        }
+    }
+}
