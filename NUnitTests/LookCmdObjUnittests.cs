e3671b1612077d5f004a1c4605c730381c968291
Karan Shah <karanshah0206@outlook.com>
2021-09-18 05:26:59 +0530

    Look Command Unit Tests Added

--- NUnitTests/LookCmdObjUnittests.cs
+++ NUnitTests/LookCmdObjUnittests.cs
@@ -8,9 +8,9 @@ namespace NUnitTests
     {
         private Look lookCommand;
         private Player player;
-        private Item i1 = new Item(new string[] { "shovel" }, "wooden shovel", "A Weak Digging Tool");
-        private Item i2 = new Item(new string[] { "spear", "javelin" }, "jade spear", "A dead deity's priceless weapon");
-        private Bag sBag = new Bag(new string[] { "b1", "bag1" }, "Spear Bag", "A bag that contains spears.");
+        private Item i1 = new Item(new string[] { "gem" }, "ruby trinket", "A priceless magical gem");
+        private Item i2 = new Item(new string[] { "spear", "javelin" }, "jade spear", "A dead deity's weapon");
+        private Bag bag = new Bag(new string[] { "bag" }, "spear bag", "A bag that contains spears.");
 
         [SetUp]
         public void Setup()
@@ -18,13 +18,64 @@ namespace NUnitTests
             lookCommand = new Look();
             player = new Player("Odin", "Widely Revered Mythical King of Norse Gods");
             player.Inventory.Put(i1);
-            sBag.Inventory.Put(i2);
-            player.Inventory.Put(sBag);
+            bag.Inventory.Put(i2);
+            player.Inventory.Put(bag);
         }
 
         [Test]
         public void TestLookAtMe()
         {
+            Assert.AreEqual(player.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "inventory" }));
+            Assert.AreEqual(player.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "me" }));
+        }
+
+        [Test]
+        public void TestLookAtGem()
+        {
+            Assert.AreEqual(i1.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "gem" }));
+        }
+
+        [Test]
+        public void TestLookAtUnk()
+        {
+            Assert.AreEqual("I can't find the diamond.", lookCommand.Execute(player, new string[] { "look", "at", "diamond" }));
+        }
+
+        [Test]
+        public void TestLookAtGemInMe()
+        {
+            Assert.AreEqual(i1.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "gem", "in", "inventory" }));
+            Assert.AreEqual(i1.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "gem", "in", "me" }));
+        }
+
+        [Test]
+        public void TestLookAtGemInBag()
+        {
+            Assert.AreEqual(i2.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "spear", "in", "bag" }));
+            Assert.AreEqual(i2.FullDescription, lookCommand.Execute(player, new string[] { "look", "at", "javelin", "in", "bag" }));
+        }
+
+        [Test]
+        public void TestLookAtGemInNoBag()
+        {
+            Assert.AreEqual("I can't find the nobag.", lookCommand.Execute(player, new string[] { "look", "at", "javelin", "in", "nobag" }));
+        }
+
+        [Test]
+        public void TestLookAtNoGemInBag()
+        {
+            Assert.AreEqual("I can't find the gem.", lookCommand.Execute(player, new string[] { "look", "at", "gem", "in", "bag" }));
+        }
+
+        [Test]
+        public void TestInvalidLook()
+        {
+            Assert.AreEqual("I don't know how to look at that.", lookCommand.Execute(player, new string[] { }));
+            Assert.AreEqual("I don't know how to look at that.", lookCommand.Execute(player, new string[] { "testing" }));
+            Assert.AreEqual("I don't know how to look at that.", lookCommand.Execute(player, new string[] { "testing", "for", "four", "words" }));
+            Assert.AreEqual("Error in look input.", lookCommand.Execute(player, new string[] { "hello", "from", "Melbourne" }));
+            Assert.AreEqual("What do you want to look at?", lookCommand.Execute(player, new string[] { "look", "randomly", "anywhere" }));
+            Assert.AreEqual("What do you want to look in?", lookCommand.Execute(player, new string[] { "look", "at", "fish", "on", "sea" }));
         }
     }
 }
