279dba2edcf52b70bbe49db2d7528c055b873aac
Karan Shah <karanshah0206@outlook.com>
2021-09-18 03:02:47 +0530

    Bag Object Created To Store Items In Items

---
+++ Swin-Adventure/Bag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swin_Adventure
+{
+    public class Bag : Item
+    {
+        private Inventory _inventory = new Inventory();
+
+        public Bag(string[] idents, string name, string desc) : base(idents, name, desc)
+        { }
+
+        public Game Locate(string id)
+        {
+            if (this.AreYou(id)) return this;
+            return _inventory.Fetch(id);
+        }
+
+        public override string FullDescription
+        {
+            get { return "In the " + Name + " you can see:\n" + _inventory.ItemList; }
+        }
+
+        public Inventory Inventory
+        {
+            get { return _inventory; }
+        }
+    }
+}
