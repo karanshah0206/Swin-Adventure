2b5162d4a1c4c0a1686cb5b6b15012e0a123b775
Karan Shah <karanshah0206@outlook.com>
2021-09-23 16:28:00 +0530

    Made Program.cs Modular

--- Swin-Adventure/Program.cs
+++ Swin-Adventure/Program.cs
@@ -6,44 +6,50 @@ namespace Swin_Adventure
     {
         static void Main(string[] args)
         {
-            string name, desc;
-
-            // Get Player Details
-            do
-            {
-                Console.Clear();
-                Console.Write("\x1b[1mEnter Player's Name:\x1b[0m ");
-                name = Console.ReadLine().Trim();
-                Console.Write("\x1b[1mEnter {0}'s Description:\x1b[0m ", name);
-                desc = Console.ReadLine().Trim();
-                Console.WriteLine("");
-            } while (name == "" || desc == "");
-
-            // Create Player Object + Add Basic Items
-            Player player = new Player(name, desc);
-            Item i1 = new Item(new string[] { "shovel" }, "Shovel", "Tool to dig or break boxes");
-            Item i2 = new Item(new string[] { "sword" }, "Sword", "Weapon for self-defence");
-            player.Inventory.Put(i1); player.Inventory.Put(i2);
-            // Add A Bag
-            Bag bag = new Bag(new string[] { "gembag" }, "Gem Bag", "Bag to store collected gems");
-            player.Inventory.Put(bag);
-            // Add A Gem To The Bag
-            Item i3 = new Item(new string[] { "ruby" }, "Ruby Gem", "Priceless little red stone");
-            bag.Inventory.Put(i3);
-
-            // Loop Look Command
+            string[] playerDetails = GetPlayerDetails();
+            Player player = InitializePlayer(playerDetails[0], playerDetails[1]);
             Look look = new Look();
-            while (true)
-            {
-                Console.Write("\x1b[1mCommand>\x1b[0m ");
 
-                Console.ForegroundColor = ConsoleColor.Red;
+            while (true) {
+                Console.Write("Command> ");
                 string[] command = Console.ReadLine().Trim().Split(' ');
-                Console.ForegroundColor = ConsoleColor.Gray;
-
                 string response = look.Execute(player, command);
                 Console.WriteLine(response);
             }
+        }
+
+        static string[] GetPlayerDetails()
+        {
+            string name, desc;
+
+            do {
+                Console.Clear();
+
+                Console.Write("Enter Player's Name: ");
+                name = Console.ReadLine().Trim();
+
+                Console.Write("Enter {0}'s Description: ", name);
+                desc = Console.ReadLine().Trim();
+
+                Console.WriteLine("");
+            } while (name == "" || desc == "");
+
+            return new string[] { name, desc };
+        }
+
+        static Player InitializePlayer(string name, string desc)
+        {
+            Player player = new Player(name, desc);
+
+            Item i1 = new Item(new string[] { "shovel" }, "Shovel", "Tool to dig or break boxes");
+            Item i2 = new Item(new string[] { "sword" }, "Sword", "Weapon for self-defence");
+            Item i3 = new Item(new string[] { "ruby" }, "Ruby Gem", "Priceless little red stone");
+            Bag bag = new Bag(new string[] { "gembag" }, "Gem Bag", "Bag to store collected gems");
+            
+            player.Inventory.Put(i1); player.Inventory.Put(i2);
+            bag.Inventory.Put(i3); player.Inventory.Put(bag);
+
+            return player;
         }
     }
 }
