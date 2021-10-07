e5373d79ae6870636784b77d33d4680640b41b14
Karan Shah <karanshah0206@outlook.com>
2021-10-07 10:35:19 +0530

    Added Path Class To Traverse Between Locations

---
+++ Swin-Adventure/Path.cs
@@ -0,0 +1,24 @@
+﻿namespace Swin_Adventure
+{
+    public class Path : Game
+    {
+        private Location _from, _destination;
+        private bool _pathLocked;
+
+        public Path(string[] ids, string name, string desc, Location from, Location dest) : base (ids, name, desc)
+        {
+            _from = from;
+            _destination = dest;
+            _pathLocked = false;
+        }
+
+        public Location From
+        { get { return _from; } }
+
+        public Location Desetination
+        { get { return _destination; } }
+
+        public bool IsTraversable
+        { get { return _pathLocked; } }
+    }
+}
