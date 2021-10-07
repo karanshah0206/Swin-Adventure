d8b34b13947596fe15b38dd523901fcf770351cc
Karan Shah <karanshah0206@outlook.com>
2021-10-07 10:42:57 +0530

    Integrated Path Into Location Class

--- Swin-Adventure/Location.cs
+++ Swin-Adventure/Location.cs
@@ -1,10 +1,14 @@
-﻿namespace Swin_Adventure
+﻿using System.Collections.Generic;
+
+namespace Swin_Adventure
 {
     public class Location : Game, IHaveInventory
     {
         private Inventory _inventory = new Inventory();
-        public Location(string[] ids, string name, string desc) : base (ids, name, desc)
-        { }
+        private List<Path> _exitPaths;
+        private Path _entryPath;
+        public Location(string[] ids, string name, string desc, Path entryPath) : base (ids, name, desc)
+        { _entryPath = entryPath; }
 
         public Game Locate(string id)
         {
@@ -12,6 +16,18 @@
             if (_inventory.HasItem(id)) { return _inventory.Fetch(id); }
             return null;
         }
+
+        public Path FindPath(string id)
+        {
+            foreach (Path p in _exitPaths) if (p.AreYou(id)) return p;
+            return null;
+        }
+
+        public void AddPath(Path path)
+        { _exitPaths.Add(path); }
+
+        public Path LeavePath
+        { get { return _entryPath; } }
 
         public Inventory Inventory
         { get { return _inventory; } }
