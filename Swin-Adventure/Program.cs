3d13f2b483849086c0bdc8023dee347c5d83ec01
Karan Shah <karanshah0206@outlook.com>
2021-10-07 11:28:59 +0530

    Improved Program Class Organization

--- Swin-Adventure/Program.cs
+++ Swin-Adventure/Program.cs
@@ -6,42 +6,40 @@ namespace Swin_Adventure
     {
         static void Main(string[] args)
         {
-            Location loc = new Location(new string[] { "base" }, "Base", "A Starting Point For All Players.");
-            string[] playerDetails = GetPlayerDetails();
-            Player player = InitializePlayer(playerDetails[0], playerDetails[1], loc);
-            Look look = new Look();
-
-            while (true) {
-                Console.Write("Command> ");
-                string[] command = Console.ReadLine().Trim().Split(' ');
-                string response = look.Execute(player, command);
-                Console.WriteLine(response);
-            }
+            Path p = CreatePath();
+            Player player = CreatePlayer(p.From);
+            RecurringCommands(player);
         }
 
-        static string[] GetPlayerDetails()
+        static Path CreatePath()
+        {
+            Location start = new Location(new string[] { "base", "start" }, "Base", "A Starting Point For All Players.");
+            Location secretRoom = new Location(new string[] { "secret" }, "Secret Room", "A Room Of Hidden Mysteries.");
+            Path tunnel = new Path(new string[] { "north", "tunnel" }, "Tunnel", "Connects Base To Secret Room.", start, secretRoom);
+            secretRoom.LeavePath = tunnel;
+            return tunnel;
+        }
+
+        static Player CreatePlayer(Location loc)
         {
             string name, desc;
 
             do {
                 Console.Clear();
-
                 Console.Write("Enter Player's Name: ");
                 name = Console.ReadLine().Trim();
-
                 Console.Write("Enter {0}'s Description: ", name);
                 desc = Console.ReadLine().Trim();
-
                 Console.WriteLine("");
             } while (name == "" || desc == "");
 
-            return new string[] { name, desc };
+            Player player = new Player(name, desc, loc);
+            InitializePlayer(player);
+            return player;
         }
 
-        static Player InitializePlayer(string name, string desc, Location loc)
+        static void InitializePlayer(Player player)
         {
-            Player player = new Player(name, desc, loc);
-
             Item i1 = new Item(new string[] { "shovel" }, "Shovel", "Tool to dig or break boxes");
             Item i2 = new Item(new string[] { "sword" }, "Sword", "Weapon for self-defence");
             Item i3 = new Item(new string[] { "ruby" }, "Ruby Gem", "Priceless little red stone");
@@ -49,8 +47,19 @@ namespace Swin_Adventure
             
             player.Inventory.Put(i1); player.Inventory.Put(i2);
             bag.Inventory.Put(i3); player.Inventory.Put(bag);
+        }
 
-            return player;
+        static void RecurringCommands(Player player)
+        {
+            Look look = new Look();
+
+            while (true)
+            {
+                Console.Write("Command> ");
+                string[] command = Console.ReadLine().Trim().Split(' ');
+                string response = look.Execute(player, command);
+                Console.WriteLine(response);
+            }
         }
     }
 }
